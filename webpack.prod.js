176475b7faf0f57e9f954ddd4cdaf0cc505ff2f0
KcirePunk <kcirepunk22misr@gmail.com>
2020-03-29 19:54:02 -0500

    Docs creado

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = {
 	},
 	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname, './dist'),
+		path: path.resolve(__dirname, './docs'),
 		filename: 'bundle.[contentHash].js'
 	},
 	module: {
